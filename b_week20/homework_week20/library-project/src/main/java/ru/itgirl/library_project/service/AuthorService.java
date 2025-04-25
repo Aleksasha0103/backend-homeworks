827a37b55d22a0db25f50b3be6bb0bc94e0bf24e
Aleksasha0103 <aleksasha0103@mail.ru>
2025-04-25 13:25:43 +0500

    Homework of the week 20

---
+++ b_week20/homework_week20/library-project/src/main/java/ru/itgirl/library_project/service/AuthorService.java
@@ -0,0 +1,18 @@
+package ru.itgirl.library_project.service;
+
+import ru.itgirl.library_project.dto.AuthorCreateDto;
+import ru.itgirl.library_project.dto.AuthorDto;
+import ru.itgirl.library_project.dto.AuthorUpdateDto;
+import ru.itgirl.library_project.model.Author;
+
+public interface AuthorService {
+    AuthorDto getAuthorById(Long id);
+    AuthorDto getAuthorBySurnameV1(String surname);
+    AuthorDto getAuthorBySurnameV2(String surname);
+    AuthorDto getAuthorBySurnameV3(String surname);
+
+    AuthorDto createAuthor(AuthorCreateDto authorCreateDto);
+    AuthorDto updateAuthor(AuthorUpdateDto authorUpdateDto);
+
+    void deleteAuthor(Long id);
+}
