a357ebc56c3c93b27d8c9bdb39c9c2cf7fa096e5
Aleksasha0103 <aleksasha0103@mail.ru>
2025-05-02 23:28:48 +0500

    Homework of the week 21

---
+++ b_week21/library-project_lesson-example/library-project/src/main/java/ru/itgirl/library_project/service/AuthorService.java
@@ -0,0 +1,21 @@
+package ru.itgirl.library_project.service;
+
+import ru.itgirl.library_project.dto.AuthorCreateDto;
+import ru.itgirl.library_project.dto.AuthorDto;
+import ru.itgirl.library_project.dto.AuthorUpdateDto;
+
+import java.util.List;
+
+public interface AuthorService {
+    AuthorDto getAuthorById(Long id);
+    AuthorDto getAuthorBySurnameV1(String surname);
+    AuthorDto getAuthorBySurnameV2(String surname);
+    AuthorDto getAuthorBySurnameV3(String surname);
+
+    AuthorDto createAuthor(AuthorCreateDto authorCreateDto);
+    AuthorDto updateAuthor(AuthorUpdateDto authorUpdateDto);
+
+    void deleteAuthor(Long id);
+
+    List<AuthorDto> getAllAuthors();
+}
