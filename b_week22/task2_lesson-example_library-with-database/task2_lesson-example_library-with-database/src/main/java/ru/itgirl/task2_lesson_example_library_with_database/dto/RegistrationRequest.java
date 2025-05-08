8b9bfbd34918e3c313b2e376a32e8fa70a320218
Aleksasha0103 <aleksasha0103@mail.ru>
2025-05-08 18:38:17 +0500

    Week 22. Example from lesson. Task 2

---
+++ b_week22/task2_lesson-example_library-with-database/task2_lesson-example_library-with-database/src/main/java/ru/itgirl/task2_lesson_example_library_with_database/dto/RegistrationRequest.java
@@ -0,0 +1,2 @@
+package ru.itgirl.task2_lesson_example_library_with_database.dto;public class RegistrationRequest {
+}
---
+++ b_week22/task2_lesson-example_library-with-database/task2_lesson-example_library-with-database/src/main/java/ru/itgirl/task2_lesson_example_library_with_database/repository/AppUserRepository.java
@@ -0,0 +1,2 @@
+package ru.itgirl.task2_lesson_example_library_with_database.repository;public interface AppUserRepository {
+}
